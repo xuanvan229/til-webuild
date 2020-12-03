d1bdfbd261fe0cc07f910877f5bcedee768bf03d
Dat Nguyen <dat.nguyen@periplus.ch>
2020-12-03 22:53:21 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import './tailwind.css';
+import data from './data.json'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App p-4 md:p-10 flex justify-center overflow-x-hidden">
+        <div className="message_container">
+          {data.map((item, index)  => (
+            <div className="item hover:bg-gray-200 rounded-md mt-2" key={item.date + index}>
+              <div className="top">
+                  <strong>{item.name}</strong>
+                  <span className="date text-gray-500">{item.date}</span>
+                </div>
+                <div dangerouslySetInnerHTML={{__html: item.message}}>
+                </div>
+            </div>
+          ))}
+        </div>
     </div>
   );
 }
